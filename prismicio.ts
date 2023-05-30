1243f86d89f96663a3188466496770968137892b
abby.liu <abby.liu@jkos.com>
2023-05-30 09:57:48 +0800

    chore: remove path

--- prismicio.ts
+++ prismicio.ts
@@ -14,16 +14,7 @@ export const repositoryName = config.repositoryName;
  * {@link https://prismic.io/docs/route-resolver#route-resolver}
  */
 // TODO: Update the routes array to match your project's route structure.
-const routes: prismic.ClientConfig["routes"] = [
-  {
-    type: "homepage",
-    path: "/",
-  },
-  {
-    type: "page",
-    path: "/:uid",
-  },
-];
+const routes: prismic.ClientConfig["routes"] = [];
 
 /**
  * Creates a Prismic client for the project's repository. The client is used to
