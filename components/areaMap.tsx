c7a216941cbbe6c586681dccd0ec9dfc41e96286
abby.liu <abby.liu@jkopay.com>
2024-03-26 05:59:30 +0800

    feat: new map

--- components/areaMap.tsx
+++ components/areaMap.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import { PrismicRichText } from "@prismicio/react";
-import MapModal from "./mapModal";
-import { get } from "lodash";
+import React, { useState } from 'react';
+import { PrismicRichText } from '@prismicio/react';
+import MapModal from './mapModal';
+import { get } from 'lodash';
 
 const AreaMap = ({ area }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [targetImg, setTargetImg] = useState("");
-  const areaData = get(area, "data.slices");
-  const title = get(area, "data.title");
+  const [targetImg, setTargetImg] = useState('');
+  const areaData = get(area, 'data.slices');
+  const title = get(area, 'data.title');
 
   const toObject = (arr: []) => {
     let obj = {};
@@ -18,7 +18,7 @@ const AreaMap = ({ area }) => {
   const mapTarget = toObject(areaData);
 
   const handleClick = (city) => {
-    setTargetImg(mapTarget?.[city] || "");
+    setTargetImg(mapTarget?.[city] || '');
     setIsOpen(true);
   };
 
@@ -35,81 +35,71 @@ const AreaMap = ({ area }) => {
 
   return (
     <>
-      <div className="relative md:px-[50px] py-20 lg:flex lg:items-center">
+      <div className="relative md:px-[60px] py-20 lg:flex lg:items-center">
         <div className="text-center">
           <PrismicRichText
             key="title"
             field={title}
             components={{
-              heading1: ({ children }) => (
-                <h1 className="text-2xl md:text-4xl leading-10">{children}</h1>
-              ),
-              heading2: ({ children }) => (
-                <h2 className="text-xl md:text-3xl leading-10">{children}</h2>
-              ),
-              heading3: ({ children }) => (
-                <h3 className="text-lg md:text-2xl leading-8">{children}</h3>
-              ),
-              heading4: ({ children }) => (
-                <h4 className="text-base md:text-xl">{children}</h4>
-              ),
-              heading5: ({ children }) => (
-                <h5 className="text-sm md:text-lg">{children}</h5>
-              ),
-              heading6: ({ children }) => (
-                <h6 className="text-sm md:text-lg">{children}</h6>
-              ),
-              paragraph: ({ children }) => (
-                <p className="text-xs md:text-base">{children}</p>
-              ),
+              heading1: ({ children }) => <h1 className="text-2xl md:text-4xl leading-10">{children}</h1>,
+              heading2: ({ children }) => <h2 className="text-xl md:text-3xl leading-10">{children}</h2>,
+              heading3: ({ children }) => <h3 className="text-lg md:text-2xl leading-8">{children}</h3>,
+              heading4: ({ children }) => <h4 className="text-base md:text-xl">{children}</h4>,
+              heading5: ({ children }) => <h5 className="text-sm md:text-lg">{children}</h5>,
+              heading6: ({ children }) => <h6 className="text-sm md:text-lg">{children}</h6>,
+              paragraph: ({ children }) => <p className="text-xs md:text-base">{children}</p>,
             }}
           />
         </div>
-        <div className="relative m-12 lg:ml-16">
+        <div className="relative m-[80px] lg:ml-14">
           <img src="/images/citymap_outline.png" alt="map" className="" />
-          <Tag city="梓官" position="top-[22%] left-[-3%]" />
-          <Tag city="楠梓" position="top-[33%] left-[-3%]" />
-          <Tag city="左營" position="top-[38%] left-[-3%]" />
-          <Tag city="鼓山" position="top-[44%] left-[-3%]" />
-          <Tag city="三民" position="top-[50%] left-[-3%]" />
-          <Tag city="鹽埕" position="top-[55%] left-[-3%]" />
-          <Tag city="前金" position="top-[61%] left-[-3%]" />
-          <Tag city="新興" position="top-[66.5%] left-[-3%]" />
-          <Tag city="前鎮" position="top-[73.5%] left-[3%]" />
-          <Tag city="小港" position="top-[83%] left-[2%]" />
-          <Tag city="岡山" position="top-[2.5%] right-[25%]" />
-          <Tag city="橋頭" position="top-[12%] right-[26%]" />
-          <Tag city="大社" position="top-[19.5%] right-[17%]" />
-          <Tag city="仁武" position="top-[26.5%] right-[19%]" />
-          <Tag city="鳥松" position="top-[33.5%] right-[21%]" />
-          <Tag city="屏東" position="top-[43.5%] right-[-7%]" />
-          <Tag city="鳳山" position="top-[58%] right-[13%]" />
-          <Tag city="苓雅" position="top-[63.5%] right-[-3%]" />
-          <Tag city="大寮" position="top-[74%] right-[6%]" />
-          <Tag city="林園" position="top-[87%] right-[12%]" />
+          <Tag city="梓官" position="top-[22.5%] left-[-3%]" />
+          <Tag city="楠梓" position="top-[34%] left-[-3%]" />
+          <Tag city="左營" position="top-[40%] left-[-3%]" />
+          <Tag city="鼓山" position="top-[46%] left-[-3%]" />
+          <Tag city="三民" position="top-[51.5%] left-[-3%]" />
+          <Tag city="鹽埕" position="top-[56.5%] left-[-3%]" />
+          <Tag city="前金" position="top-[62.5%] left-[-3%]" />
+          <Tag city="新興" position="top-[67.5%] left-[-3%]" />
+          <Tag city="前鎮" position="top-[72.5%] left-[-3%]" />
+          <Tag city="小港x大林浦" position="top-[77%] left-[-4%]" />
+          <Tag city="小港-大林浦" position="top-[83.1%] left-[-4%]" />
+          <Tag city="旗津" position="top-[90.5%] left-[5%]" />
+
+          <Tag city="岡山" position="top-[7.5%] right-[14%]" />
+          <Tag city="橋頭" position="top-[15.5%] right-[14%]" />
+          <Tag city="大社" position="top-[21.5%] right-[14%]" />
+          <Tag city="仁武" position="top-[27.4%] right-[14%]" />
+          <Tag city="屏東" position="top-[42.6%] right-[-7%]" />
+          <Tag city="鳥松" position="top-[48.8%] right-[-7%]" />
+          <Tag city="鳳山" position="top-[58.2%] right-[-7%]" />
+          <Tag city="苓雅" position="top-[64.6%] right-[-7%]" />
+          <Tag city="大寮一" position="top-[73.5%] right-[12%]" />
+          <Tag city="大寮二" position="top-[78.5%] right-[12%]" />
+          <Tag city="林園" position="top-[87.6%] right-[12%]" />
         </div>
-        <div className="relative m-12 lg:ml-8 lg:scale-[0.9]">
+        <div className="relative m-[80px] lg:ml-4">
           <img src="/images/specialmap_outline.png" alt="map" className="" />
-          <Tag city="阿蓮" position="top-[1%] left-[34%]" />
-          <Tag city="湖內" position="top-[-4%] left-[-2%]" />
-          <Tag city="路竹" position="top-[19%] left-[-3%]" />
-          <Tag city="彌陀" position="top-[30%] left-[4%]" />
-          <Tag city="燕巢" position="top-[42.5%] left-[9%]" />
-          <Tag city="麟洛" position="top-[60.5%] left-[14%]" />
-          <Tag city="萬丹" position="top-[67.5%] left-[16%]" />
-          <Tag city="新園" position="top-[74.5%] left-[15%]" />
-          <Tag city="東港" position="top-[91%] left-[19%]" />
-          <Tag city="美濃" position="top-[15%] right-[3%]" />
-          <Tag city="旗山" position="top-[28%] right-[16%]" />
-          <Tag city="大樹" position="top-[37%] right-[16%]" />
-          <Tag city="長治" position="top-[39.5%] right-[-2%]" />
-          <Tag city="內埔" position="top-[51.5%] right-[-1%]" />
-          <Tag city="竹田" position="top-[60%] right-[-1%]" />
-          <Tag city="萬巒" position="top-[66.5%] right-[-2%]" />
-          <Tag city="潮州" position="top-[72.5%] right-[-2%]" />
-          <Tag city="崁頂" position="top-[80%] right-[1%]" />
-          <Tag city="南州" position="top-[85%] right-[11%]" />
-          <Tag city="林邊" position="top-[92.5%] right-[2%]" />
+          <Tag city="阿蓮" position="top-[25.1%] left-[-2%]" />
+          <Tag city="路竹" position="top-[29.8%] left-[-2%]" />
+          <Tag city="彌陀" position="top-[34.9%] left-[-2%]" />
+          <Tag city="湖內" position="top-[7.6%] left-[22%]" />
+          <Tag city="旗山" position="top-[50%] left-[12%]" />
+          <Tag city="長治" position="top-[56.8%] left-[12%]" />
+          <Tag city="麟洛" position="top-[64%] left-[12%]" />
+          <Tag city="萬丹" position="top-[71%] left-[12%]" />
+          <Tag city="新園" position="top-[78%] left-[12%]" />
+          <Tag city="東港" position="top-[85.2%] left-[12%]" />
+          <Tag city="燕巢" position="top-[42.6%] left-[9%]" />
+          <Tag city="美濃" position="top-[15.9%] right-[-7%]" />
+          <Tag city="大樹" position="top-[39%] right-[-7%]" />
+          <Tag city="內埔" position="top-[43.6%] right-[-7%]" />
+          <Tag city="萬巒" position="top-[58.4%] right-[-7%]" />
+          <Tag city="竹田" position="top-[64%] right-[-7%]" />
+          <Tag city="潮州" position="top-[71.2%] right-[-7%]" />
+          <Tag city="崁頂" position="top-[78.2%] right-[-7%]" />
+          <Tag city="南州" position="top-[85.2%] right-[-7%]" />
+          <Tag city="林邊" position="top-[92.3%] right-[-7%]" />
         </div>
       </div>
       <MapModal isShow={isOpen} setIsShow={setIsOpen} image={targetImg} />
--- components/mapModal.tsx
+++ components/mapModal.tsx
@@ -1,25 +1,39 @@
-import Modal from "./modal";
+import React, { useState, useEffect } from 'react';
+import Modal from './modal';
 
 const mapModal = ({ isShow, setIsShow, image }) => {
+  const [hiddenClass, setHiddenClass] = useState('');
+
+  useEffect(() => {
+    if (!isShow) setHiddenClass('');
+  }, [isShow]);
+
   return (
     <Modal isShow={isShow} setIsShow={setIsShow}>
       <div
         className={`w-[90%] max-h-[80%] overflow-scroll fixed z-[500] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-65%] md:translate-y-[-50%] ${
-          isShow ? "" : "hidden"
+          isShow ? '' : 'hidden'
         }`}
       >
-        <div
-          onClick={() => setIsShow(false)}
-          className="close cursor-pointer top-0"
-        ></div>
-        <div className="space-y-10 max-h-[600px] overflow-scroll mt-[40px]">
+        <div onClick={() => setIsShow(false)} className="close cursor-pointer top-0"></div>
+        <div className="relative space-y-10 min-h-[600px] max-h-[600px] overflow-scroll mt-[40px]">
           {image &&
-            image.map((el) => (
-              <img
-                src={el.image.url}
-                alt=""
-                className="lg:max-h-[600px] mx-auto"
-              />
+            image.map((el, index) => (
+              <>
+                {index === 0 && (
+                  <img
+                    className={`absolute top-1/3 left-1/2 w-[60px] animate-[spin_4s_linear_infinite] ${hiddenClass}`}
+                    src="/images/loadingcircle.png"
+                    alt=""
+                  />
+                )}
+                <img
+                  src={el.image.url}
+                  alt=""
+                  className={`lg:max-h-[600px] mx-auto`}
+                  onLoad={(e) => setHiddenClass('hidden')}
+                />
+              </>
             ))}
         </div>
       </div>
